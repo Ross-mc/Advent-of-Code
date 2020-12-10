9cd26bf6d8a69b423a2b0544583ff1db2b74c76e
Ross McIntyre <rossmci90@hotmail.co.uk>
2020-12-10 15:26:59 +0000

    Completed day 3 part 2

--- Day 3/path-finder.js
+++ Day 3/path-finder.js
@@ -23,40 +23,59 @@ forest.pop();
 let treeCount = 0;
 
 
-function pathFinder(row, position){
+function pathFinder(position, moveDown, moveRight){
+
+    let treeCount = 0;
+
+    for (let i = 0; i<forest.length-1; i+=moveDown){
+        var currentRow = forest[i].split("");
+
+        var nextRow = forest[i+moveDown].split("")
+        //logic to move back to the start of the row if the row is ended
+        if (position+moveRight >= currentRow.length){
+            position = moveRight-(currentRow.length-position)
+        } else {
+            position = position+moveRight
+        };
+    
+        if (nextRow[position] === "#"){
+            treeCount++
+        };
+    }
 
     //if we are are on the last row, return the count of trees
-    if (row === forest.length-1){
-        return console.log('the tree count is: ', treeCount);
-    };
-
-    var currentRow = forest[row].split("");
-
-    //we need to move 'right' by 3 and 'down' by 1, check the current position to see if it is a tree
-
-    var nextRowIndex = row+1;
-    var nextRow = forest[nextRowIndex].split("")
-    var newPosition = 0;
-    //logic to move back to the start of the row if the row is ended
-    if (position+3 >= currentRow.length){
-        newPosition = 3-(currentRow.length-position)
-    } else {
-        newPosition = position+3
-    };
-
-    console.log(nextRow);
-    console.log(newPosition);
+    return treeCount
 
 
-    if (nextRow[newPosition] === "#"){
-        console.log('Found a tree!')
-        treeCount++
-    };
     
 
-    pathFinder(nextRowIndex, newPosition)
 
 
 };
 
-pathFinder(0,0)
+var result1 = pathFinder(0, 1, 3);
+
+console.log(result1)
+
+
+// Determine the number of trees you would encounter if, for each of the following slopes, you start at the top-left corner and traverse the map all the way to the bottom:
+
+// Right 1, down 1.
+// Right 3, down 1. (This is the slope you already checked.)
+// Right 5, down 1.
+// Right 7, down 1.
+// Right 1, down 2.
+// In the above example, these slopes would find 2, 7, 3, 4, and 2 tree(s) respectively; multiplied together, these produce the answer 336.
+
+// What do you get if you multiply together the number of trees encountered on each of the listed slopes?
+
+function findMultiply(){
+    var ans1 = pathFinder(0,1,1);
+    var ans2 = pathFinder(0,1,3);
+    var ans3 = pathFinder(0,1,5);
+    var ans4 = pathFinder(0,1,7);
+    var ans5 = pathFinder(0,2,1);
+    return ans1 * ans2 * ans3 * ans4 * ans5
+}
+
+console.log(findMultiply())
